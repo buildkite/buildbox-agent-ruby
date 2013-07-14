24c9792b3c3964b27e63a2cef6945dac3aa5efa0
Keith Pitt <me@keithpitt.com>
2013-07-14 14:47:10 +1000

    Register the worker if no uuid is present.

--- lib-new/buildbox.rb
+++ lib-new/buildbox.rb
@@ -7,8 +7,8 @@ require 'pathname'
 require 'json'
 
 class Configuration < Hashie::Dash
-  property :worker_uid
-  property :endpoint, :default => "http://api.buildbox.dev/v1"
+  property :worker_uuid
+  property :api_endpoint, :default => "http://api.buildbox.dev/v1"
 
   def save
     File.open(path, 'w+') { |file| file.write(pretty_json) }
@@ -29,7 +29,7 @@ class Configuration < Hashie::Dash
   end
 
   def read_and_load
-    merge JSON.parse(path.read)
+    merge! JSON.parse(path.read)
   end
 
   def path
@@ -60,18 +60,18 @@ module API
       @config = config
     end
 
-    def user
-      get("user")
+    def register(hostname)
+      post("workers", :hostname => hostname)
     end
 
     def builds
-      get("workers/#{SecureRandom.uuid}/builds")
+      get("workers/#{@config.worker_uuid}/builds")
     end
 
     private
 
     def connection
-      @connection ||= Faraday.new(:url => @config.endpoint) do |faraday|
+      @connection ||= Faraday.new(:url => @config.api_endpoint) do |faraday|
         faraday.request  :json
 
         faraday.response :logger
@@ -85,13 +85,30 @@ module API
       end
     end
 
+    def post(path, body = {})
+      connection.post(path) do |request|
+        request.body = body
+      end.body
+    end
+
     def get(path)
       connection.get(path).body
     end
   end
 end
 
-p API::Client.new.builds
+api = API::Client.new
+
+if Buildbox.config.worker_uuid.nil?
+  worker = api.register(`hostname`.chomp)
+
+  Buildbox.config.tap do |config|
+    config.worker_uuid = worker.uuid
+    config.save
+  end
+end
+
+p api.builds
 
 =begin
 require 'celluloid'
