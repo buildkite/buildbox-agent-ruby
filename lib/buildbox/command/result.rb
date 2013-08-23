fe2b8b808e6d928f5a69245c314b8fc181bf2720
Keith Pitt <me@keithpitt.com>
2013-08-24 09:06:02 +1000

    Removing old lib files.

--- lib/buildbox/command/result.rb
+++
@@ -1,6 +0,0 @@
-# encoding: UTF-8
-
-module Buildbox
-  class Command::Result < Struct.new(:command, :output, :exit_status)
-  end
-end
--- lib/buildbox/version.rb
+++
@@ -1,5 +0,0 @@
-# encoding: UTF-8
-
-module Buildbox
-  VERSION = "0.0.4"
-end
