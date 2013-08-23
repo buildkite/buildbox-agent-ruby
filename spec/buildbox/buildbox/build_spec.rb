db1162782846e91df8e05ca44006f3ae8532aee8
Keith Pitt <me@keithpitt.com>
2013-08-24 09:38:49 +1000

    Removed old specs.

--- spec/buildbox/buildbox/build_spec.rb
+++
@@ -1,7 +0,0 @@
-# encoding: UTF-8
-
-require 'spec_helper'
-
-describe Buildbox::Build do
-  let(:build) { Buildbox::Build.new(:uuid => '1234', :repository => "git@github.com:keithpitt/ci-ruby.git", :commit => "67b15b704e0", :command => "rspec", :config => { :script => [ "rspec" ] }) }
-end
--- spec/buildbox/buildbox_spec.rb
+++
@@ -1,6 +0,0 @@
-# encoding: UTF-8
-
-require 'spec_helper'
-
-describe Buildbox do
-end
