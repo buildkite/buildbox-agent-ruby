a8f837c77df8aff9046d6b4b91eb7f995d245830
Keith Pitt <me@keithpitt.com>
2013-11-14 00:05:56 +1100

    Cleaned up some of the glob handling.

--- lib/buildbox/artifact.rb
+++ lib/buildbox/artifact.rb
@@ -15,47 +15,39 @@ module Buildbox
     end
 
     def files_to_upload
-      tmpdir           = Dir.mktmpdir
-      path_to_absolute = {}
+      tmpdir       = Dir.mktmpdir
+      file_hash    = {}
 
-      copy_files_to_upload(tmpdir).each do |file|
-        path_to_absolute[relativize_to_dir(file, tmpdir)] = file
+      globbed_files.each do |file|
+        absolute_path = File.expand_path(file, @build_directory)
+        copy_to_path  = File.join(tmpdir, file)
+
+        if File.file?(absolute_path)
+          file_hash[file] = copy_to_path
+
+          FileUtils.mkdir_p(File.dirname(copy_to_path))
+          FileUtils.cp(absolute_path, copy_to_path)
+        end
       end
 
-      path_to_absolute
+      file_hash
     end
 
     private
 
-    def copy_files_to_upload(dir)
-      expanded_directory = File.expand_path(@build_directory)
-      absolute_glob      = File.expand_path(@glob, expanded_directory)
-
-      target_files = Dir.glob(absolute_glob)
-
-      target_files.each do |file|
-        relative_path = relativize_to_dir(file, expanded_directory)
-        copy_to       = File.join(dir, relative_path)
-
-        if File.file?(file)
-          FileUtils.mkdir_p(File.dirname(copy_to))
-          FileUtils.cp(file, copy_to)
-        end
-      end
-
-      # Grab all the files we're going to upload.
-      Dir.glob(File.join(dir, "**", "*")).reject { |file| File.directory?(file) }
+    def glob_path
+      Pathname.new(@glob)
     end
 
-    # /foo/build-directory/something.txt => /something.txt
-    # /var/random/something.txt => /var/random/something.txt
-    def relativize_to_dir(path, directory)
-      if path.to_s.index(directory.to_s) == 0
-        parts = path.to_s.split(directory.to_s)
-        parts.shift
-        parts.join(directory.to_s)
+    def build_directory_path
+      Pathname.new(@build_directory)
+    end
+
+    def globbed_files
+      if glob_path.relative?
+        Dir.chdir(build_directory_path.expand_path) { Dir.glob(glob_path) }
       else
-        path
+        Dir.glob(glob_path)
       end
     end
   end
--- spec/buildbox/buildbox/artifact_spec.rb
+++ spec/buildbox/buildbox/artifact_spec.rb
@@ -15,28 +15,32 @@ describe Buildbox::Artifact do
       files = Buildbox::Artifact.files_to_upload(directory, "foo.txt")
 
       expect(files.length).to eql(1)
-      test_for_files(files, %w(/foo.txt))
+      test_for_files(files, %w(foo.txt))
     end
 
     it "handles globs" do
       files = Buildbox::Artifact.files_to_upload(directory, "bar/**/*.txt")
 
       expect(files.length).to eql(4)
-      test_for_files(files, %w(/bar/bang.txt /bar/bang1.txt /bar/bang2.txt /bar/inside-bar/bang3.txt))
+      test_for_files(files, %w(bar/bang.txt bar/bang1.txt bar/bang2.txt bar/inside-bar/bang3.txt))
     end
 
     it "handles absolute globs" do
       files = Buildbox::Artifact.files_to_upload(directory, File.join(File.expand_path(directory), "**/*.txt"))
+      expected_files = %w(foo.txt bar/bang.txt bar/bang1.txt bar/bang2.txt bar/inside-bar/bang3.txt).map do |file|
+        File.join(directory, file)
+      end
 
       expect(files.length).to eql(5)
-      test_for_files(files, %w(/foo.txt /bar/bang.txt /bar/bang1.txt /bar/bang2.txt /bar/inside-bar/bang3.txt))
+      test_for_files(files, expected_files)
     end
 
     it "handles specifying everything under a folder" do
       files = Buildbox::Artifact.files_to_upload(directory, "coverage/**/*")
 
       expect(files.length).to eql(25)
-      test_for_files(files, %w(/coverage/index.html /coverage/assets/0.8.0/application.js))
+      test_for_files(files, %w(coverage/index.html coverage/assets/0.8.0/application.js))
     end
   end
+
 end
