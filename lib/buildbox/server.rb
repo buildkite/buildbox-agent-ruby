7f02afc63b9f429ee2786bf08f6019337bd627be
Keith Pitt <me@keithpitt.com>
2014-01-24 01:36:16 +0800

    More profilling.

--- lib/buildbox/server.rb
+++ lib/buildbox/server.rb
@@ -8,6 +8,7 @@ module Buildbox
       @config = config
       @logger = logger
       @supervisors = []
+      @iterations = 0
     end
 
     def start
@@ -20,52 +21,22 @@ module Buildbox
         @logger.info "Agent with access token `#{access_token}` has started."
       end
 
+      before = ObjectSpace.count_objects
+
       loop do
-        @supervisors.each do |supervisor|
-          supervisor.actors.first.async.process
-        end
-
-        GC.start
-
-        # Generate a new set of objects
-        if ENV['PROFILE']
-          objects = Hash.new(0)
-
-          ObjectSpace.each_object do |o|
-            objects[o.class] += 1
+        x = allocate_count(before) do
+          @supervisors.each do |supervisor|
+            supervisor.actors.first.async.process
           end
 
-          if !@previous_objects.nil?
-            diff = {}
+          # https://github.com/ruby-prof/ruby-prof
 
-            objects.each_pair do |key, new_value|
-              old_value = @previous_objects[key]
+          GC.start
 
-              if old_value > new_value
-                diff[key] = "+#{old_value - new_value} (#{new_value})"
-              elsif new_value < old_value
-                diff[key] = "-#{new_value - old_value} (#{new_value})"
-              else
-                # diff[key] = old_value
-              end
-            end
-
-            puts "=== OBJECT COUNT CHANGES ==="
-            diff.each_pair do |key, value|
-              puts "#{key}: #{value}"
-            end
-            puts "============================"
-          else
-            sorted_objects = objects.sort_by { |key, value| value }.reverse
-            sorted_objects.each do |pair|
-              puts "#{pair[0]}: #{pair[1]}"
-            end
-          end
-
-          @previous_objects = objects.dup
+          wait INTERVAL
         end
 
-        wait INTERVAL
+        p x
       end
     end
 
@@ -79,6 +50,15 @@ module Buildbox
 
     def agent_access_tokens
       @config.agent_access_tokens
+    end
+
+    def allocate_count(before, &block)
+      GC.disable
+      yield
+      after = ObjectSpace.count_objects
+      after.each { |k,v| after[k] = v - before[k] }
+      GC.enable
+      after
     end
   end
 end
